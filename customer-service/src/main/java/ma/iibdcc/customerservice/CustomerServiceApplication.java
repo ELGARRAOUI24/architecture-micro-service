25ad85dd4e2476fe3dec80e7ca97dfb6d4df63da
Bougarra <bougarra.02@gmail.com>
2026-04-04 16:54:35 +0100

    Créer le micro-service customer-service qui permet de gérer les client

---
+++ customer-service/src/main/java/ma/iibdcc/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,29 @@
+package ma.iibdcc.customerservice;
+
+import ma.iibdcc.customerservice.entities.Customer;
+import ma.iibdcc.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(Customer.builder()
+					.name("Imane").email("imane@gmail.com").build());
+			customerRepository.save(Customer.builder()
+					.name("Hassan").email("hassan@gmail.com").build());
+			customerRepository.save(Customer.builder()
+					.name("Amine").email("amine@gmail.com").build());
+		};
+	}
+
+}
