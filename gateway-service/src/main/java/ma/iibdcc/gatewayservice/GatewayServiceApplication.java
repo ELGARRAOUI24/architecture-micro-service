464b92dd3e089d7aafada5ba5f14572e81999a75
Bougarra <bougarra.02@gmail.com>
2026-04-05 11:18:50 +0100

    Faire une configuration dynamique des routes de la gateway

--- gateway-service/src/main/java/ma/iibdcc/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ma/iibdcc/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package ma.iibdcc.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +16,7 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
+    //@Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("r1", p -> p.path("/customers/**")
@@ -21,5 +24,12 @@ public class GatewayServiceApplication {
                 .route("r2", p -> p.path("/customers/**")
                         .uri("lb://INVENTORY-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamiqueRoutes(
+            ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp
+    ){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 }
